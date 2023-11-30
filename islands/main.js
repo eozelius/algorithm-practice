1ef9af2a3a2fb2808ad20f580e191fa7efb2c7d3
ethan ozelius <e.ozelius@gmail.com>
2023-11-30 15:24:59 -0700

    stable

--- islands/main.js
+++ islands/main.js
@@ -1,100 +1,93 @@
+const traverseNeighbors = (cell, grid, directionsToCheck = [], map) => {
+  const [ row, col ] = cell;
+  
+  console.log(`   >>>> [ traverseNeighbors ] => [ ${row}, ${col} ]`)
+
+  // UP
+  if (row > 0 && directionsToCheck.includes('UP')) {
+    if (grid[row - 1][col] === '1' && map[row - 1][col].traversed === false) {
+      console.log(`         >>>> [ traverseNeighbors ] UP!! invoking traverserNeighbors for cell: [ ${row - 1 }, ${col} ]`)
+      map[row - 1][col].traversed = true;
+      traverseNeighbors([row - 1, col], grid, ['UP', 'RIGHT', 'LEFT'], map)
+    }
+  }
+
+  // DOWN
+  if (row < grid.length - 1 && directionsToCheck.includes('DOWN')) {
+    if (grid[row + 1][col] === '1' && map[row + 1][col].traversed === false) {
+      console.log(`         >>>> [ traverseNeighbors ] DOWN!! invoking traverserNeighbors for cell: [ ${row + 1}, ${col} ]`)
+      map[row + 1][col].traversed = true
+      traverseNeighbors([row + 1, col], grid, ['DOWN', 'RIGHT', 'LEFT'], map)
+    }
+  }
+
+  // RIGHT
+  if (col < grid[0].length && directionsToCheck.includes('RIGHT')) {
+    if (grid[row][col + 1] === '1' && map[row][col + 1].traversed === false) {
+      console.log(`         >>>> [ traverseNeighbors ] RIGHT!! invoking traverserNeighbors for cell: [ ${row}, ${col + 1} ]`)
+      map[row][col + 1].traversed = true
+      traverseNeighbors([row, col + 1], grid, ['DOWN', 'RIGHT', 'UP'], map)
+    }
+  }
+
+  // LEFT
+  if (col > 0 && directionsToCheck.includes('LEFT') ) {
+    if (grid[row][col - 1] === '1' && map[row][col - 1].traversed === false) {
+      console.log(`         >>>> [ traverseNeighbors ] LEFT!! invoking traverserNeighbors for cell: [ ${row}, ${col - 1} ]`)
+      map[row][col - 1].traversed = true
+      traverseNeighbors([row, col - 1], grid, ['DOWN', 'RIGHT', 'LEFT'])
+    }
+  }
+
+}
+
 /**
  * @param {character[][]} grid
  * @return {number}
  *
  * 
-Input: grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-Output: 1
-
-
-Input: grid = [
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]
-
-[ 0 0 0]
-[ 0 1 0]
-[ 0 0 0]
-
-0,0 => { cell: true, island: 1, traversed: true, neighbors: { up: false, right: true, left: false, down: true } }
-0,1 => { cell: true, island: 1, traversed: true, neighbors: { up: false, right: false, left: true, down: true } }
-0,2 => { cell: false, island: null, traversed: true, neighbors: { up: false, right: false, left: true, down: false } }
-
-
-Output: 3
-
-* 
- * 
- * 
- * 
-
-
  */
 var numIslands = function(grid) {
+  console.log(`###### grid ######`)
+  console.log(grid)
+  
   let islands = 0;
+
+  const map = []
+
+  for (let i = 0; i < grid.length; i++) {
+    map.push([])
+    for (let j = 0; j < grid[i].length; j++) {
+      map[i].push({
+        coor: [i,j],
+        isLand: grid[i][j] === '1',
+        traversed: false,
+        id: Math.floor(Math.random() * 9999)
+      })
+    }
+  }
 
   // nested for loop to iter each cell
   for (let i = 0; i < grid.length; i++) {
+    // add a new row to map
     for (let j = 0; j < grid[i].length; j++) {
       // is current cell land?
       const isLand = grid[i][j] === '1';
+      const isAlreadyTraversed = map[i][j].traversed;
 
       // if yes -> check top, right, bot, left neighbors to see if there is land
-      if (isLand) {
+      if (isLand && isAlreadyTraversed === false) {
+        map[i][j].traversed = true;
         console.log(`## [ ${i}, ${j}] LAND HO!!!!!!!!! #####`)
-        let hasLandNeighbor = false;
-                
-        // UP
-        if (i > 0) {
-          console.log('chekcing UP')
-          const cellAbove = grid[i - 1][j]
-          if (cellAbove === '1') {
-            hasLandNeighbor = true;
-          }
-        }
-        
-        // RIGHT
-        if(j < grid[0].length) {
-          console.log('chekcing RIGHT')
-
-          const cellRight = grid[i][j + 1]
-          if (cellRight === '1') {
-            hasLandNeighbor = true;
-          }
-        }
-
-        // LEFT
-        if (j > 0) {
-          console.log('chekcing LEFT')
-
-          const cellLeft = grid[i][j - 1]
-          if (cellLeft === '1') {
-            hasLandNeighbor = true;
-          }
-        }
-
-        // BOTTOM
-        if (i < grid.length) {
-          console.log('chekcing BOTTOM')
-          const cellDown = grid[i + 1][j]
-          if (cellDown === '1') {
-            hasLandNeighbor = true;
-          }
-        }
-
-        console.log(`hasLandNeighbor => `, hasLandNeighbor)
-
-
-        if (!hasLandNeighbor) {
-          islands++
-        }
+        islands++
+        // console.log('JSON.stringify(map) => ', JSON.stringify(map, null, 4))
+        traverseNeighbors([i,j], grid, ['UP', 'DOWN', 'RIGHT', 'LEFT'], map)
+      } else if (isLand && isAlreadyTraversed === true) {
+        // is land, but has already been accounted for as part of another island
+        // i.e. do nothing
+      } else {
+        // is not land, i.e. mark as traversed, continue on.
+        map[i][j].traversed = true;
       }
     }
   }
@@ -102,65 +95,21 @@ var numIslands = function(grid) {
   return islands;
 };
 
-
-// var checkNeighbors = (cell, grid, directionsToCheck = [ 'UP', 'RIGHT', 'BOTTOM', 'LEFT' ]) => {
-//   const [row, col] = cell;
-//   console.log(`[ main ] checkNeighbors() [ ${row}, ${col} ]`)
-
-//   const neighbors = {
-//     UP: false,
-//     DOWN: false,
-//     RIGHT: false,
-//     LEFT: false,
-//   }
-  
-//   // UP
-//   // Check row must be at least 2nd row, for top be checked.
-//   if (row > 0 && directionsToCheck.includes('UP')) {
-//     const cellAbove = grid[row - 1][col]
-//     if (cellAbove === '1') {
-//       neighbors['UP'] = true
-//     }
-//   }
-
-//   // RIGHT
-//   // Check col must be at most 2nd to last col
-//   if (col < grid[0].length && directionsToCheck.includes('RIGHT')) {
-//     const cellRight = grid[row][col + 1]
-//     if (cellRight === '1') {
-//       neighbors['RIGHT'] = true
-//     }
-//   }
-
-//   // DOWN
-//   // Check row must be at most 2nd to last row
-//   if (row < grid.length && directionsToCheck.includes('DOWN')) {
-//     const cellDown = grid[row + 1][col]
-//     if (cellDown === '1') {
-//       neighbors['DOWN'] = true
-//     }
-//   }
-
-//   // LEFT
-//   // Check row must be at most 2nd to last row
-//   if (col > 0 && directionsToCheck.includes('LEFT')) {
-//     const cellLeft = grid[row][col - 1]
-//     if (cellLeft === '1') {
-//       neighbors['LEFT'] = true
-//     }
-//   }
-
-//   console.log(`[ Mian ] checkNeighbors() neighbors => `, neighbors)
-
-//   return neighbors
-  
-// }
-
-
 const ex1 = [
-  [ '0', '0', '0' ],
-  [ '0', '1', '1' ],
-  [ '0', '0', '0' ],
+  [ '1', '1', '0', '0' ],
+  [ '1', '1', '0', '0' ],
+  [ '0', '0', '1', '1' ],
+  [ '0', '1', '0', '1' ],
+  [ '1', '0', '0', '0' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
+  [ '0', '0', '0', '1' ],
 ]
 
 const r1 = numIslands(ex1)
